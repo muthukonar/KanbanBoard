fd74704db0c4c4670f26e9c71fe2ca8728ae2e4d
pechikonar <muthukonaruot@gmail.com>
2025-02-14 16:45:37 -0500

    Code updated for middleware auth.ts for verifying the token

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,19 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token) {
+    return res.status(403).json({ message: 'Access denied, no token provided' });
+  }
+
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+
+    req.user = decoded as JwtPayload; 
+next();
+return();
+  }catch (err) {
+    return res.status(403).json({message: 'Invalid token'});
+  }
 };
