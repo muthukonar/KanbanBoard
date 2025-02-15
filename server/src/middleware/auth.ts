c83fce5c6400744f044ffdf96b0fcfa55cae081e
pechikonar <muthukonaruot@gmail.com>
2025-02-15 11:06:42 -0500

    middleware auth.ts refactored the code

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,21 +5,29 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
-  const token = req.headers['authorization']?.split(' ')[1];
-
-  if (!token) {
-    return res.status(403).json({ message: 'Access denied, no token provided' });
-  }
-
-  try {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-
-    req.user = decoded as JwtPayload; 
-next();
-// return;
-  }catch (err) {
-    return res.status(403).json({message: 'Invalid token'});
-  }
-};
+  export const authenticateToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const authHeader = req.headers.authorization;
+  
+    if (authHeader) {
+      const token = authHeader.split(' ')[1];
+  
+      const secretKey = process.env.JWT_SECRET_KEY || '';
+  
+      jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+          return res.sendStatus(403); // Forbidden
+        }
+  
+        req.user = user as JwtPayload;
+        return next();
+      });
+    } else {
+      res.sendStatus(401); // Unauthorized
+    }
+  };
+  
